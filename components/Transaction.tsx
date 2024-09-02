0d7d38978056003a9efbb900d06be0159bb1249f
Chintan Shah <chintans98@gmail.com>
2024-09-02 01:18:13 -0700

    transactions component built out

--- components/Transaction.tsx
+++ components/Transaction.tsx
@@ -1,58 +1,82 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import commonStyles from '../styles/commonStyles';
-import BrandingColours from '../styles/brandingConstants';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { NewBrandingColours } from '../styles/brandingConstants';
 import { AppTransaction } from '../models/lunchmoney/appModels';
-import CategoryComponent from './Category';
 import { formatAmountString } from '../data/formatBalance';
 
 type TransactionProps = {
   transaction: AppTransaction;
 };
 
-const transactionStyles = StyleSheet.create({
-  card: {
-    ...commonStyles.card,
-    borderWidth: 0,
-    backgroundColor: BrandingColours.backgroundColour,
+const styles = StyleSheet.create({
+  transactionItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: NewBrandingColours.neutral.lightGray,
   },
-  leftSection: {
-    flex: 3.5,
-    flexDirection: 'column',
-  },
-  rightSection: {
+  transactionMain: {
+    flexDirection: 'row',
+    alignItems: 'center',
     flex: 1,
-    alignItems: 'flex-end',
-    flexDirection: 'column',
+  },
+  transactionIcon: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 12,
+  },
+  transactionDetails: {
+    flex: 1,
   },
   transactionName: {
-    flexWrap: 'wrap',
-    flexShrink: 1,
-    color: BrandingColours.darkTextColour,
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: '500',
+    color: NewBrandingColours.text.primary,
+    marginBottom: 2,
   },
-  date: {
-    ...commonStyles.textBase,
-    color: 'grey',
+  transactionCategory: {
+    fontSize: 14,
+    color: NewBrandingColours.text.secondary,
+    marginBottom: 2,
+  },
+  transactionAccount: {
+    fontSize: 12,
+    color: NewBrandingColours.text.muted,
+  },
+  transactionSecondary: {
+    alignItems: 'flex-end',
+  },
+  transactionAmount: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginBottom: 2,
+  },
+  transactionDate: {
+    fontSize: 12,
+    color: NewBrandingColours.text.muted,
+  },
+  pendingTag: {
     fontSize: 10,
+    color: NewBrandingColours.accent.orange,
+    fontWeight: '600',
+    paddingHorizontal: 6,
+    paddingVertical: 2,
+    backgroundColor: `${NewBrandingColours.accent.orange}20`,
+    borderRadius: 4,
   },
-  account: {
-    ...commonStyles.textBase,
-    color: 'grey',
-    fontSize: 10,
-  },
-  amount: {
-    color: BrandingColours.secondaryColour,
-    fontWeight: 'bold',
-  },
-  amountNegative: {
-    color: BrandingColours.red,
-  },
-  amountPositive: {
-    color: BrandingColours.green,
+  splitIcon: {
+    marginTop: 2,
   },
 });
+
+const getCategoryName = (transaction: AppTransaction) => {
+  return transaction.categoryName ? transaction.categoryName : 'Uncategorized';
+};
 
 const getAssetName = (transaction: AppTransaction) => {
   if (transaction.isGrouped) {
@@ -62,57 +86,136 @@ const getAssetName = (transaction: AppTransaction) => {
   return transaction.assetName ? transaction.assetName : 'unknown account';
 };
 
+// TODO: have a IconInfo, separate into different class with all the mappings
+const getCategoryIcon = (category: string): string => {
+  const lowerCaseCategory = category?.toLowerCase() || 'Uncategorized';
+
+  if (
+    lowerCaseCategory.includes('food') ||
+    lowerCaseCategory.includes('drink')
+  ) {
+    return 'coffee';
+  }
+  if (lowerCaseCategory.includes('transportation')) {
+    return 'truck';
+  }
+  if (lowerCaseCategory.includes('shopping')) {
+    return 'shopping-bag';
+  }
+  if (
+    lowerCaseCategory.includes('bills') ||
+    lowerCaseCategory.includes('utilities')
+  ) {
+    return 'file-text';
+  }
+  if (lowerCaseCategory.includes('entertainment')) {
+    return 'film';
+  }
+  if (
+    lowerCaseCategory.includes('income') ||
+    lowerCaseCategory.includes('deposit')
+  ) {
+    return 'dollar-sign';
+  }
+  if (lowerCaseCategory.includes('Uncategorized')) {
+    return 'help-circle';
+  }
+  return 'circle';
+};
+
+const getCategoryColor = (category: string): string => {
+  const lowerCaseCategory = category?.toLowerCase() || 'Uncategorized';
+
+  if (
+    lowerCaseCategory.includes('food') ||
+    lowerCaseCategory.includes('drink')
+  ) {
+    return NewBrandingColours.accent.orange;
+  }
+  if (lowerCaseCategory.includes('transportation')) {
+    return NewBrandingColours.primary.main;
+  }
+  if (lowerCaseCategory.includes('shopping')) {
+    return NewBrandingColours.accent.purple;
+  }
+  if (
+    lowerCaseCategory.includes('bills') ||
+    lowerCaseCategory.includes('utilities')
+  ) {
+    return NewBrandingColours.accent.red;
+  }
+  if (lowerCaseCategory.includes('entertainment')) {
+    return NewBrandingColours.accent.yellow;
+  }
+  if (
+    lowerCaseCategory.includes('income') ||
+    lowerCaseCategory.includes('deposit')
+  ) {
+    return NewBrandingColours.secondary.main;
+  }
+  if (lowerCaseCategory.includes('Uncategorized')) {
+    return NewBrandingColours.accent.red;
+  }
+  return NewBrandingColours.neutral.gray;
+};
+
 function TransactionComponent({ transaction }: TransactionProps) {
   const transactionAmount = parseFloat(transaction.amount);
   const transactionAmountString = formatAmountString(transactionAmount);
 
   return (
-    <View style={transactionStyles.card}>
-      <View style={transactionStyles.leftSection}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Text
-            numberOfLines={1}
-            ellipsizeMode="tail"
-            style={transactionStyles.transactionName}
-          >
-            {transaction.payee}
-          </Text>
-
-          {transaction.isSplit ? (
-            <MaterialIcons
-              style={{ marginLeft: 5 }}
-              name="call-split"
-              size={16}
-              color={BrandingColours.secondaryColour}
-            />
-          ) : null}
-
-          {transaction.status === 'pending' ? (
-            <CategoryComponent categoryName="pending" />
-          ) : null}
-
-          <CategoryComponent categoryName={transaction.categoryName} />
+    <TouchableOpacity style={styles.transactionItem}>
+      <View style={styles.transactionMain}>
+        <View
+          style={[
+            styles.transactionIcon,
+            { backgroundColor: getCategoryColor(transaction.categoryName) },
+          ]}
+        >
+          <Icon
+            name={getCategoryIcon(transaction.categoryName)}
+            size={20}
+            color={NewBrandingColours.neutral.white}
+          />
         </View>
-        <View style={{ flexDirection: 'row' }}>
-          <Text style={transactionStyles.account}>
+        <View style={styles.transactionDetails}>
+          <Text style={styles.transactionName}>{transaction.payee}</Text>
+          <Text style={styles.transactionCategory}>
+            {getCategoryName(transaction)}
+          </Text>
+          <Text style={styles.transactionAccount}>
             {getAssetName(transaction)}
           </Text>
         </View>
       </View>
-      <View style={transactionStyles.rightSection}>
+      <View style={styles.transactionSecondary}>
         <Text
           style={[
-            transactionStyles.amount,
-            transactionAmount >= 0
-              ? transactionStyles.amountPositive
-              : transactionStyles.amountNegative,
+            styles.transactionAmount,
+            {
+              color:
+                transactionAmount >= 0
+                  ? NewBrandingColours.secondary.main
+                  : NewBrandingColours.accent.red,
+            },
           ]}
         >
           {transactionAmountString}
         </Text>
-        <Text style={transactionStyles.date}>{transaction.date}</Text>
+        <Text style={styles.transactionDate}>{transaction.date}</Text>
+        {transaction.status === 'pending' && (
+          <Text style={styles.pendingTag}>Pending</Text>
+        )}
+        {transaction.isSplit && (
+          <Icon
+            name="git-branch"
+            size={16}
+            color={NewBrandingColours.accent.purple}
+            style={styles.splitIcon}
+          />
+        )}
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
 
--- screens/Transactions.tsx
+++ screens/Transactions.tsx
@@ -5,40 +5,15 @@ import {
   FlatList,
   TouchableOpacity,
   StyleSheet,
-  StatusBar,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
 import { useParentContext } from '../context/app/appContextProvider';
-import { AppTransaction } from '../models/lunchmoney/appModels';
-import { formatAmountString } from '../data/formatBalance';
 import { NewBrandingColours } from '../styles/brandingConstants';
-
-function getCategoryIcon(category) {
-  switch (category?.toLowerCase()) {
-    case 'food':
-      return 'coffee';
-    case 'transportation':
-      return 'truck';
-    case 'income':
-      return 'dollar-sign';
-    case 'utilities':
-      return 'zap';
-    case 'shopping':
-      return 'shopping-bag';
-    case 'entertainment':
-      return 'film';
-    case 'health':
-      return 'heart';
-    default:
-      return 'circle';
-  }
-}
+import TransactionComponent from '../components/Transaction';
+import commonStyles from '../styles/commonStyles';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: NewBrandingColours.neutral.background,
-    paddingTop: StatusBar.currentHeight,
+    ...commonStyles.container,
   },
   header: {
     flexDirection: 'row',
@@ -58,20 +33,23 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     padding: 16,
+    marginHorizontal: 16,
+    borderRadius: 8,
     backgroundColor: NewBrandingColours.neutral.white,
-    // borderBottomWidth: 1,
-    // borderBottomColor: NewBrandingColours.neutral.lightGray,
   },
   sortLabel: {
     fontSize: 16,
     color: NewBrandingColours.text.secondary,
-    marginRight: 8,
+    // marginRight: 8,
+  },
+  sortButtonsContainer: {
+    flexDirection: 'row',
   },
   sortButton: {
-    paddingVertical: 6,
-    paddingHorizontal: 12,
-    borderRadius: 16,
-    marginHorizontal: 4,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 20,
+    marginLeft: 8,
     backgroundColor: NewBrandingColours.neutral.lightGray,
   },
   sortButtonActive: {
@@ -87,38 +65,6 @@ const styles = StyleSheet.create({
   transactionList: {
     paddingHorizontal: 16,
   },
-  transactionItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingVertical: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: NewBrandingColours.neutral.lightGray,
-  },
-  transactionIcon: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    // backgroundColor: NewBrandingColours.neutral.lightGray,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 12,
-  },
-  transactionDetails: {
-    flex: 1,
-  },
-  transactionName: {
-    fontSize: 16,
-    fontWeight: '500',
-    color: NewBrandingColours.text.primary,
-  },
-  transactionDate: {
-    fontSize: 14,
-    color: NewBrandingColours.text.muted,
-  },
-  transactionAmount: {
-    fontSize: 16,
-    fontWeight: '600',
-  },
 });
 
 export default function TransactionsScreen() {
@@ -132,98 +78,49 @@ export default function TransactionsScreen() {
     return Math.abs(parseFloat(b.amount)) - Math.abs(parseFloat(a.amount));
   });
 
-  // TODO: better mapping
-  const getCategoryColor = (): string => {
-    const accentColors = Object.values(NewBrandingColours.accent);
-    const randomIndex = Math.floor(Math.random() * accentColors.length);
-    return accentColors[randomIndex];
-  };
-
-  const renderTransaction = (transaction: AppTransaction) => (
-    <TouchableOpacity style={styles.transactionItem}>
-      <View
-        style={[
-          styles.transactionIcon,
-          { backgroundColor: getCategoryColor() },
-        ]}
-      >
-        <Icon
-          name={getCategoryIcon(transaction.categoryName)}
-          size={20}
-          color={NewBrandingColours.neutral.white}
-        />
-      </View>
-      <View style={styles.transactionDetails}>
-        <Text style={styles.transactionName}>{transaction.payee}</Text>
-        <Text style={styles.transactionDate}>{transaction.date}</Text>
-      </View>
-      <Text
-        style={[
-          styles.transactionAmount,
-          {
-            color:
-              parseFloat(transaction.amount) >= 0
-                ? NewBrandingColours.secondary.main
-                : NewBrandingColours.accent.red,
-          },
-        ]}
-      >
-        {formatAmountString(transaction.amount)}
-      </Text>
-    </TouchableOpacity>
-  );
-
   return (
     <View style={styles.container}>
-      {/* <View style={styles.header}>
-        {/* <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Icon name="arrow-left" size={24} color="#4A5568" />
-        </TouchableOpacity>
-         <Text style={styles.headerTitle}>Transactions</Text>
-        <TouchableOpacity onPress={() => {}}>
-          <Icon name="filter" size={24} color="#4A5568" />
-        </TouchableOpacity>
-      </View> */}
-
       <View style={styles.sortContainer}>
         <Text style={styles.sortLabel}>Sort by:</Text>
-        <TouchableOpacity
-          style={[
-            styles.sortButton,
-            sortBy === 'date' && styles.sortButtonActive,
-          ]}
-          onPress={() => setSortBy('date')}
-        >
-          <Text
+        <View style={styles.sortButtonsContainer}>
+          <TouchableOpacity
             style={[
-              styles.sortButtonText,
-              sortBy === 'date' && styles.sortButtonTextActive,
+              styles.sortButton,
+              sortBy === 'date' && styles.sortButtonActive,
             ]}
+            onPress={() => setSortBy('date')}
           >
-            Date
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            styles.sortButton,
-            sortBy === 'amount' && styles.sortButtonActive,
-          ]}
-          onPress={() => setSortBy('amount')}
-        >
-          <Text
+            <Text
+              style={[
+                styles.sortButtonText,
+                sortBy === 'date' && styles.sortButtonTextActive,
+              ]}
+            >
+              Date
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
             style={[
-              styles.sortButtonText,
-              sortBy === 'amount' && styles.sortButtonTextActive,
+              styles.sortButton,
+              sortBy === 'amount' && styles.sortButtonActive,
             ]}
+            onPress={() => setSortBy('amount')}
           >
-            Amount
-          </Text>
-        </TouchableOpacity>
+            <Text
+              style={[
+                styles.sortButtonText,
+                sortBy === 'amount' && styles.sortButtonTextActive,
+              ]}
+            >
+              Amount
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
 
       <FlatList
         data={sortedTransactions}
-        renderItem={({ item }) => renderTransaction(item)}
+        renderItem={({ item }) => <TransactionComponent transaction={item} />}
         keyExtractor={item => item.id.toString()}
         contentContainerStyle={styles.transactionList}
       />
